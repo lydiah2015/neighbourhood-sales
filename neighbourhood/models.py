09154821357f82200a9950c2a3f53682976ec401
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-06-03 06:30:00 +0300

    created basic models

---
+++ neighbourhood/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    pass
+
+class Neighbourhood(models.Model):
+    pass
+
+class Business(models.Model):
+    pass
+
+class Contact(models.Model):
+    pass
+
+class Posts(models.Model):
+    pass
