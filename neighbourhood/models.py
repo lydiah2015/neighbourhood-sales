496095f938a187737cdba082852e3571515107d2
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-06-04 11:30:24 +0300

    model methods- some required stuff

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Profile(models.Model):
     user=models.OneToOneField(User,
                 related_name="profile",
@@ -46,6 +45,31 @@ class Neighbourhood(models.Model):
 
     def __str__(self):
         return "{}".format(self.name if self.name else self.id)
+    
+    @classmethod
+    def create_neighbourhood(cls,name):
+        n=Neighbourhood(name=name)
+        n.save()
+        return n
+    
+    def delete_neigborhood(self):
+        Neighbourhood.object.get(pk=self.pk).delete()
+
+    @classmethod
+    def find_neigborhood(cls,neigborhood_id):
+        try:
+            return Neighbourhood.objects.get(id=neigborhood_id)
+        except Neighbourhood.DoesNotExist:
+            return None
+
+    def update_neighborhood(self,name,location):
+        self.name=name
+        self.location=location
+        self.save()
+    
+    def update_occupants(self,occ):
+        self.occupants.add(occ)
+        self.save()
 
 class Business(models.Model):
     name=models.CharField(max_length=255, null=False)
@@ -58,8 +82,25 @@ class Business(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     inserted_at = models.DateTimeField(auto_now=True)
 
+    @classmethod
+    def create_business(cls):
+        pass
+
+    def delete_business(self):
+        self.delete()
+
+    def find_business(self,business_id):
+        return Business.objects.get(pk=business_id)
+
+    def update_business(self,name,description):
+        self.name=name
+        self.description=description
+        self.save()
+
     def __str__(self):
         return "business:{}".format(self.name if self.name else self.id)
+
+
 
 class Contact(models.Model):
     name=models.CharField(max_length=255, null=False)
@@ -97,3 +138,4 @@ class Post(models.Model):
 
     def __str__(self):
         return "Post:{}".format(self.id)
+    
